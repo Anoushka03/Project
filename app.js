9067647bcb0bdd26a8232571500ebe0050fc5be8
Souvik <souvikde.ns@gmail.com>
2020-05-01 19:59:42 +0530

    ejs and public folder created

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+// requiring modules 
 const express = require('express')
+const ejs = require('ejs')
 
 
 // middlewares 
 const app = express()
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.raw())
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+
+// ====== 
 
 app.get('/',(req,res)=>{
-    res.send('Hello World')
+    res.render('index',{})
 })
+
+
 
 app.listen(process.env.PORT||3000,()=>{
     console.log(`Server started at port ${process.env.PORT||3000}`)
